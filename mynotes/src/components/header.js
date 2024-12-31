98141cf837cfcb63e82fef162d6e301ad1a61694
skymonil <158536159+skymonil@users.noreply.github.com>
2024-12-31 14:50:13 +0530

    Update header.js

--- mynotes/src/components/header.js
+++ mynotes/src/components/header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="app-header">
-            <h1>My Notes Github Webhooks69</h1>
+            <h1>My Notes Github Webhooks99</h1>
         </div>
     )
 }
